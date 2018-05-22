1ddc0cfd3061f8f6d9f4d7ed8a4408944b5e9eb6
Tofandel <adrien.foulon.lfgeb@gmail.com>
2018-05-22 16:50:21 +0200

    Initial

--- tgmpa-config.php
+++ tgmpa-config.php
@@ -31,7 +31,7 @@
  * Plugin:
  * require_once dirname( __FILE__ ) . '/path/to/class-tgm-plugin-activation.php';
  */
-require_once dirname( __FILE__ ) . '/TGMPluginActivation/class-tgm-plugin-activation.php';
+require_once dirname( __FILE__ ) . 'vendor/tgmpa/tgm-plugin-activation/class-tgm-plugin-activation.php';
 
 add_action( 'tgmpa_register', 'wplusplus_register_required_plugins' );
 
