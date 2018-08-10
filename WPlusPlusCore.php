00313733c6c7eb745ecae113bd9a7fdc694589a5
Tofandel <adrien.foulon.lfgeb@gmail.com>
2018-08-10 13:34:49 +0200

    Copyright

--- WPlusPlusCore.php
+++ WPlusPlusCore.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Copyright (c) 2018.
  * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
  * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
  * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
