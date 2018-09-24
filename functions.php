55a767d71aedb58d468354af330cb6bfd99da0ae
Tofandel <adrien.foulon.lfgeb@gmail.com>
2018-09-24 13:24:17 +0200

    Added featured image and Link columns

--- functions.php
+++ functions.php
@@ -7,6 +7,25 @@
 
 //namespace Tofandel;
 
+if ( ! function_exists( 'wpp_array_insert_after' ) ) {
+	/**
+	 * Insert a value or key/value pair after a specific key in an array.  If key doesn't exist, value is appended
+	 * to the end of the array.
+	 *
+	 * @param array $array
+	 * @param string $key
+	 * @param array $new
+	 *
+	 * @return array
+	 */
+	function wpp_array_insert_after( array $array, $key, array $new ) {
+		$keys  = array_keys( $array );
+		$index = array_search( $key, $keys );
+		$pos   = false === $index ? count( $array ) : $index + 1;
+
+		return array_merge( array_slice( $array, 0, $pos ), $new, array_slice( $array, $pos ) );
+	}
+}
 if ( ! function_exists( 'wpp_admin_notice' ) ) {
 	/**
 	 * @param $text
